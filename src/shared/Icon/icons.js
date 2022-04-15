02c3df461b2e86cba5b2eb3b0112fb9e4179d17d
hsangwon <101050184+hsangwon@users.noreply.github.com>
2022-04-15 23:09:10 +0900

    header&side UI

---
+++ src/shared/Icon/icons.js
@@ -0,0 +1,18 @@
+import { HiHome } from "react-icons/hi";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import { FaRegPlusSquare } from "react-icons/fa";
+import { ImCompass2 } from "react-icons/im";
+import { FiHeart } from "react-icons/fi";
+import { BsPersonCircle, BsSearch } from "react-icons/bs";
+
+const headerIcon = {
+  HiHome,
+  IoPaperPlaneOutline,
+  FaRegPlusSquare,
+  ImCompass2,
+  FiHeart,
+  BsPersonCircle,
+  BsSearch,
+};
+
+export { headerIcon };
